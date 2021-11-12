dab6a7a0683ce5ea277d45b3eeba60ec6788cdae
Paulo Couto <1200587@isep.ipp.pt>
2021-11-12 12:14:30 +0000

    LAP22G87-15 #menu [Implementation]

--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -1,19 +1,9 @@
 package lapr.project.ui;
 
-import lapr.project.model.CalculatorExample;
-import lapr.project.model.Ship;
-//import lapr.project.model.ShipIMO;
-import lapr.project.model.Ship;
-import lapr.project.model.ShipBST;
 import lapr.project.store.ShipStore;
-import lapr.project.utils.BST;
-import lapr.project.utils.CsvReader;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
 /**
@@ -39,37 +29,30 @@ class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        CalculatorExample calculatorExample = new CalculatorExample();
-        int value = calculatorExample.sum(3, 5);
-
-        if (LOGGER.isLoggable(Level.INFO)) {
-            LOGGER.log(Level.INFO, String.valueOf(value));
-        }
-
-        //US 101 - Print the 3 bsts (avls)
-        //US 103 - The 3 avls are with the date ordered
         ShipStore st = new ShipStore();
-        ArrayList<Ship> shipArray;
-        shipArray = CsvReader.readCSV();
-        ShipBST shipBst = new ShipBST();
+        String option = new String();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        while (!option.equals("0")) {
+            System.out.println("Please make your selection");
+            System.out.println("1) Import Data");
+            System.out.println("2) Search ship");
+            System.out.println("0) Leave");
+            option = read.readLine();
+            switch (option) {
+                case "1":
+                    st.insert();
+                    break;
+                case "2":
+                    System.out.println("Insert the value: ");
+                    String value = read.readLine();
+                    System.out.println(st.findShip(value));
 
-        shipBst.insert();
-        shipBst.printTrees();
-
-        int cont=0;
-        for (Ship s: shipArray) {
-            cont++;
-            System.out.println(s.toString());
+                    break;
+                case "0":
+                    System.out.println("bye");
+                    break;
+            }
         }
-        System.out.println(cont);
-
-       st.insert();
-        st.print();
-        System.out.println(st.findShip("210950000"));
-        System.out.println(st.findShip("9395044"));
-        System.out.println(st.findShip("C4SQ2"));
-
-
     }
 }
 
