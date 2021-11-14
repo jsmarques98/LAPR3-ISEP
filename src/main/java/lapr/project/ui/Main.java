4b5e3eda17bd457c26f4fa6756fd4ff1d9376299
jsmarques98 <jsmarques2011@gmail.com>
2021-11-14 20:24:15 +0000

    LAP22G87-15 #UI modification [implementation]

--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -32,29 +32,21 @@ class Main {
     public static void main(String[] args) throws Exception {
         ShipStore st = new ShipStore();
         String option = new String();
-        CsvReader cs = new CsvReader();
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         while (!option.equals("0")) {
             System.out.println("Please make your selection");
-            System.out.println("1) Read CSV");
-            System.out.println("2) Search ship");
-            System.out.println("3) Search ship summary");
+            System.out.println("1) Search ship");
+            System.out.println("2) Search ship summary");
             System.out.println("0) Leave");
             option = read.readLine();
             String value;
-            String path;
-            switch (option) {
+            switch (option){
                 case "1":
-                    System.out.println("Insert the name file: ");
-                    path = read.readLine();
-                    System.out.println(cs.readCSV("src/main/java/lapr/project/data/"+path+".csv"));
-                    break;
-                case "2":
                     System.out.println("Insert the value: ");
                     value = read.readLine();
                     System.out.println(st.findShip(value));
                     break;
-                case "3":
+                case "2":
                     System.out.println("Insert the value: ");
                     value = read.readLine();
                     System.out.println(st.shipSummary(value));
