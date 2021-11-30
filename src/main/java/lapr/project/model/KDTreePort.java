7a154f446eab6d38d66a5fc500412d2d4468759a
jsmarques98 <jsmarques2011@gmail.com>
2021-11-30 12:07:20 +0000

    LAP22G87-29 # Created the class KDTreePort to create a port 2d tree [Implementation]

---
+++ src/main/java/lapr/project/model/KDTreePort.java
@@ -0,0 +1,35 @@
+package lapr.project.model;
+
+import lapr.project.model.Port;
+import lapr.project.utils.KDTree;
+import lapr.project.utils.CsvReader;
+import lapr.project.utils.NodeKDTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KDTreePort {
+
+    KDTree portTree = new KDTree();
+
+    ArrayList<Port> portArray = new ArrayList<>();
+    {
+        try {
+            String path = "src/main/java/lapr/project/data/sports.csv";
+            portArray = CsvReader.readPorts(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private  void insertPorts() {
+        List<NodeKDTree<Port>> nodes = new ArrayList<>();
+        for (Port port : portArray) {
+            NodeKDTree<Port> node = new NodeKDTree<Port>(port, port.getLat(), port.getLon());
+            nodes.add(node);
+        }
+        portTree.buildTree(nodes);
+    }
+
+
+}
