54c45835b0a0bb9e2dad93db210350253f42fa7f
jsmarques98 <jsmarques2011@gmail.com>
2021-12-02 12:09:53 +0000

    LAP22G87-35 # Tested the readPorts method [Tests]

--- src/test/java/lapr/project/utils/CsvReaderTest.java
+++ src/test/java/lapr/project/utils/CsvReaderTest.java
@@ -1,6 +1,7 @@
 package lapr.project.utils;
 
 import lapr.project.model.DynamicShip;
+import lapr.project.model.Port;
 import lapr.project.model.Ship;
 import org.junit.jupiter.api.Test;
 
@@ -11,35 +12,68 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CsvReaderTest {
 
-        String path = "testReader";
+    String path = "testReader";
+    String path1 = "portReaderTest";
 
-        private final Ship s1;
-        ArrayList<Ship> shipArray = new ArrayList<>();
-        ArrayList<DynamicShip> shipDataArray = new ArrayList<>();
-        LocalDateTime t1 = LocalDateTime.of(2020, 12, 31, 17, 03, 00);
-        LocalDateTime t2 = LocalDateTime.of(2020, 12, 31, 17, 19, 00);
-        DynamicShip ds1 = new DynamicShip(t1, 42.92236, -66.97243, 12.5, 2.4, 358, 'B');
-        DynamicShip ds2 = new DynamicShip(t2, 42.97875, -66.97001, 12.9, 13.1, 355, 'B');
+    private final Ship s1;
+    private final Port p1;
 
-        public CsvReaderTest() {
-            s1 = new Ship(210950000, shipDataArray, "VARAMO", 9395044, "C4SQ2", 70, 166, 25, 9.5, 0) {
-            };
-            shipDataArray.add(ds2);
-            shipDataArray.add(ds1);
-        }
-        
-    @Test
-        void readCSV() throws Exception {
-            shipArray = CsvReader.readCSV(path);
-            ArrayList<Ship> expectRes = shipArray;
-            assertEquals(expectRes, shipArray);
-        }
+    ArrayList<Ship> shipArray = new ArrayList<>();
+    ArrayList<DynamicShip> shipDataArray = new ArrayList<>();
+    ArrayList<Port> portArray = new ArrayList<>();
+    LocalDateTime t1 = LocalDateTime.of(2020, 12, 31, 17, 03, 00);
+    LocalDateTime t2 = LocalDateTime.of(2020, 12, 31, 17, 19, 00);
+    DynamicShip ds1 = new DynamicShip(t1, 42.92236, -66.97243, 12.5, 2.4, 358, 'B');
+    DynamicShip ds2 = new DynamicShip(t2, 42.97875, -66.97001, 12.9, 13.1, 355, 'B');
 
 
-        @Test
-        void sortByDate() throws Exception {
-            shipArray = CsvReader.readCSV(path);
-            ArrayList<Ship> expectRes = shipArray;
-            assertEquals(expectRes, shipArray);
-        }
+    public CsvReaderTest() {
+        s1 = new Ship(210950000, shipDataArray, "VARAMO", 9395044, "C4SQ2", 70, 166, 25, 9.5, 0) {
+        };
+        shipDataArray.add(ds2);
+        shipDataArray.add(ds1);
+
+        p1 = new Port("Europe", "United Kingdom", 29002, "Liverpool", 53.46666667, -3.033333333);
+        portArray.add(p1);
     }
+
+    @Test
+    void readCSV() throws Exception {
+        shipArray = CsvReader.readCSV(path);
+        ArrayList<Ship> expectRes = shipArray;
+        assertEquals(expectRes, shipArray);
+    }
+
+
+    @Test
+    void sortByDate() throws Exception {
+        shipArray = CsvReader.readCSV(path);
+        ArrayList<Ship> expectRes = shipArray;
+        assertEquals(expectRes, shipArray);
+    }
+
+    // Teste correto pois tem o to string igual, mas diz que não está igual
+    /*@Test
+    void readPorts() throws Exception {
+        ArrayList<Port> portArray1 = CsvReader.readPorts(path1);
+        ArrayList<Port> expectRes = portArray1;
+        assertEquals(expectRes, portArray);
+    }*/
+
+
+    @Test
+    void readPorts() throws Exception {
+
+        ArrayList<Port> portArray1 = CsvReader.readPorts(path1);
+        portArray1.get(0).toString();
+        assertEquals(1, portArray1.size());
+        assertEquals("Europe", portArray1.get(0).getContinent());
+        assertEquals("United Kingdom", portArray1.get(0).getCountry());
+        assertEquals(29002, portArray1.get(0).getCode());
+        assertEquals("Liverpool", portArray1.get(0).getPort());
+        assertEquals(53.46666667, portArray1.get(0).getLat());
+        assertEquals(-3.033333333, portArray1.get(0).getLon());
+
+    }
+}
+
