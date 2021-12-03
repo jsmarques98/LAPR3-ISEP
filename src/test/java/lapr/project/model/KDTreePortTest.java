9033a5335c5d708661b1d1d913e95bd1e29169da
1181122 <1181122@isep.ipp.pt>
2021-12-03 21:29:44 +0000

    LAP22G87-35 #KDTreePort testing [Implementation]

---
+++ src/test/java/lapr/project/model/KDTreePortTest.java
@@ -0,0 +1,41 @@
+package lapr.project.model;
+
+import lapr.project.model.KDTreePort;
+import lapr.project.model.Port;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KDTreePortTest {
+
+    Port port1;
+    Port port2;
+    Port port3;
+
+    public KDTreePortTest (){
+        port1 = new Port("Europe","United Kingdom", 29002,"Liverpool",53.46666667,-3.033333333);
+        port2 = new Port("America","United States",14635,"Los Angeles",33.71666667,-118.2666667);
+        port3 = new Port("America","United States",25007,"New Jersey",40.66666667,-74.16666667);
+    }
+
+    @Test
+    public void testInsertPorts(){
+        KDTreePort portTree = new KDTreePort();
+        ArrayList<Port> portArray = new ArrayList<>();
+        portArray.add(port1);
+        portArray.add(port2);
+        portArray.add(port3);
+
+        portTree.insertPorts();
+
+        Port node = portTree.nearestPort(53.46666667,-3.033333333);
+        assertEquals(port1, node);
+        node = portTree.nearestPort(33.71666667,-118.2666667);
+        assertEquals(port2, node);
+        node = portTree.nearestPort(34.21971, -5.10125);
+        assertEquals(port3, node);
+    }
+}
