1e20ddaae0f20680bc24843347b03b39aa2e53e7
Paulo Couto <1200587@isep.ipp.pt>
2021-12-29 17:03:33 +0000

    LAP22G87-73 #Bug fix Test [Implementation]

--- src/test/java/lapr/project/model/MaterialTest.java
+++ src/test/java/lapr/project/model/MaterialTest.java
@@ -42,9 +42,9 @@ class MaterialTest {
     void testHashCode(){
         assertEquals(-301671663,m.hashCode());
     }
-/*
+
     @Test
     void testToString() {
         assertEquals("Aco With Thermal Conductivity 52,000 W/mk, good for Outside layer",m.toString());
-    }*/
+    }
 }
