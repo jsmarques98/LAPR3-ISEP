487df026cf5ad6e0879644de2767092a38684b9b
jsmarques98 <jsmarques2011@gmail.com>
2022-01-22 19:09:52 +0000

    LAP22G87-80 # Test class [Implementation]

---
+++ src/test/java/lapr/project/utils/CircuitTest.java
@@ -0,0 +1,68 @@
+package lapr.project.utils;
+
+import lapr.project.data.*;
+import lapr.project.model.Position;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircuitTest {
+    PositionMatrixGraph pmg;
+    CountryStore cs;
+    PortStore ps;
+    SeaDistStore sds;
+    BorderStore bs;
+    KDTreePort kdt;
+    Circuit circ;
+
+
+    @BeforeEach
+    public void initializeGraph() {
+        pmg = new PositionMatrixGraph();
+        cs = new CountryStore();
+        kdt = new KDTreePort();
+        sds = new SeaDistStore();
+        ps = new PortStore(kdt);
+        bs = new BorderStore();
+        circ = new Circuit();
+        pmg.fillMatrixGraph(3,cs.getCountryList(),ps.getPortList(),sds.getSeaDistArrayList(),bs.toMap(bs.getBorderArray(),cs.getCountryArray()));
+    }
+
+    @Test
+    void mostEfficientCircuit(){
+        assertEquals("Tbilisi\n" +
+                "Ankara\n" +
+                "Athens\n" +
+                "Tirana\n" +
+                "Skopje\n" +
+                "Pristina\n" +
+                "Podgorica\n" +
+                "Sarajevo\n" +
+                "Belgrade\n" +
+                "Sofia\n" +
+                "Bucharest\n" +
+                "Chisinau\n" +
+                "Kyiv\n" +
+                "Budapest\n" +
+                "Zagreb\n" +
+                "Ljubljana\n" +
+                "Rome\n" +
+                "Bern\n" +
+                "Vaduz\n" +
+                "Vienna\n" +
+                "Bratislava\n" +
+                "Prague\n" +
+                "Warsaw\n" +
+                "Minsk\n" +
+                "Vilnius\n" +
+                "Riga\n" +
+                "Tallinn\n" +
+                "Moscow\n",circ.mostEfficientCircuit(pmg.getCompleteMap(),"funchal","funchal"));
+    }
+
+
+}
